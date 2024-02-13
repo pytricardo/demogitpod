4acb91b126cc9b86309c7585a3fc17321c1764b4
Ricardo <38985223+pytricardo@users.noreply.github.com>
2024-02-13 08:51:48 +0100

    Add files via upload

---
+++ src/main/java/com/example/demogitpod/DemogitpodApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogitpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitpodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitpodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demogitpod/DemogitpodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogitpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitpodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
